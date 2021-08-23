f2a67c9eb2ea4722ada173984b3385eef39f9f4e
Rhapsody0128 <rhapsody0128@gmail.com>
2021-08-23 17:18:00 +0800

    silderHalf

--- src/components/game_slider.vue
+++ src/components/game_slider.vue
@@ -1,15 +1,51 @@
 <template lang="pug">
-.game_slider
+.game_slider(:style='gameSliderStyle' v-if="life<100 && start && born!==0" )
 </template>
 <script>
-export default {};
+export default {
+  emits: ["destroy"],
+  data() {
+    return {
+      bpm: 20,
+      life: 0,
+      start: false,
+    };
+  },
+  props: {
+    color: String,
+    born: Number,
+  },
+  computed: {
+    gameSliderStyle() {
+      if (this.life > 0) {
+        return {
+          top: this.life + "%",
+          background: this.color,
+        };
+      } else {
+      }
+    },
+  },
+  methods: {
+    destroy() {
+      console.log("object");
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.lifeTimer = setInterval(() => {
+        this.life += 1;
+      }, this.bpm);
+      this.start = true;
+    }, this.born);
+  },
+};
 </script>
 <style lang="stylus" scoped>
 .game_slider
   width 100%
   height 1rem
   top 5%
-  background red
   position absolute
   border-radius 0.1rem
   padding 0
--- src/components/playboard.vue
+++ src/components/playboard.vue
@@ -2,66 +2,100 @@
 #playboard
   el-slider.slider(v-model="viewDegree" vertical height="10vh" :max='80')
   .row(:style='playBoardStyle()')
+    el-button(@click="gameStart()") start
     .full-screen(v-for='(data,index) in data')
-      .screen
+      .screen(:id="'S'+data.key")
         .judgeBar
-        game_slider
-      el-button.button.shadow(:style="getShadowStyle(data.color)" size="medium" @click="hit(key)" :id="data.key") {{data.key}}
+        game_slider(@click="destroy($event)" v-if='start' v-for='(timeStamp,index) in data.timeStamp' :class="data.key" :color='data.color' :born="timeStamp")
+      el-button.button.shadow(:style="getShadowStyle(data.color)" size="medium" @click="hit(data.key)" :id="'B'+data.key") {{data.key}}
 </template>
 <script>
 export default {
   data() {
     return {
+      a: [1, 2, 3],
+      start: false,
       viewDegree: 0,
-      keys: [],
+      startTimeStamp: 0,
       data: [
         {
           key: "a",
           color: "rgb(200,10,15)",
+          timeStamp: [100, 800],
         },
         {
           key: "s",
           color: "rgb(0,10,15)",
+          timeStamp: [5000],
         },
-        {
-          key: "d",
-          color: "rgba(0,0,0,0.5)",
-        },
-        {
-          key: " ",
-          color: "green",
-        },
-        {
-          key: "4",
-          color: "green",
-        },
-        {
-          key: "5",
-          color: "green",
-        },
-        {
-          key: "6",
-          color: "green",
-        },
+        // {
+        //   key: "d",
+        //   color: "rgba(0,0,0,0.5)",
+        //   timeStamp: [1000, 3000],
+        // },
+        // {
+        //   key: " ",
+        //   color: "green",
+        //   timeStamp: [1000, 2000],
+        // },
+        // {
+        //   key: "4",
+        //   color: "green",
+        //   timeStamp: [2000, 3000],
+        // },
+        // {
+        //   key: "5",
+        //   color: "green",
+        //   timeStamp: [1, 2000, 3000],
+        // },
+        // {
+        //   key: "6",
+        //   color: "green",
+        //   timeStamp: [1000, 2000, 3000],
+        // },
       ],
     };
   },
   computed: {},
   methods: {
+    gameStart() {
+      this.start = true;
+    },
     playBoardStyle() {
       return {
-        transform: `rotateX(${this.viewDegree}deg)`,
+        transform: `
+        rotateX(${this.viewDegree}deg)
+        scale(${1 - this.viewDegree / 170})`,
       };
     },
-
     getShadowStyle(color) {
       return {
         background: color,
       };
     },
-    hit(key) {},
+    hit(key) {
+      this.judge(key);
+      const sliders = document.getElementsByClassName(key);
+      if (sliders.length > 0) {
+        sliders[0].click();
+        console.log(sliders[0]);
+      }
+
+      // console.log(last);
+      // if (last !== null) {
+      //   last.remove();
+      // }
+    },
+    destroy(e) {
+      this.$emit("destroy", "");
+    },
+    judge(key) {
+      // const screen = document.getElementById("S" + key);
+      // screen.firstChild.remove();
+    },
     commitHit(key) {
-      const button = document.getElementById(key);
+      const button = document.getElementById("B" + key);
+      button.click();
       button.classList.add("show");
       setTimeout(() => {
         button.classList.remove("show");
@@ -70,19 +104,16 @@ export default {
   },
   mounted() {},
   created() {
-    var keys = [];
-    this.data.map((perData) => {
-      keys.push(perData.key);
-    });
     const component = this;
     document.onkeydown = function (event) {
-      keys.map((key) => {
-        if (key === event.key) {
-          component.commitHit(key);
+      component.data.map((data) => {
+        if (data.key === event.key) {
+          component.commitHit(data.key);
         }
       });
     };
   },
+  watch: {},
 };
 </script>
 <style lang="stylus" scoped>
@@ -96,31 +127,33 @@ export default {
   .slider
     position absolute
     top 5%
+    left 0
   .row
     flex-wrap nowrap
     width 100%
     display flex
+    height 100%
   .full-screen
     height 70vh
     width 100%
     position relative
     .screen
-      box-shadow 0px 0px 3px rgba(0,0,0,1)
+      box-shadow 0px 0px 1px rgba(0,0,0,1)
       height 100%
       overflow hidden
       .judgeBar
         width 100%
         height 1rem
-        background red
+        border 1px solid black
         position absolute
-        top 80%
+        top 90%
     .button
       position relative
       width 100%
       height 6rem
       text-align center
       border-width 0rem
-      box-shadow 0px 0px 3px rgba(0,0,0,1)
+      box-shadow 0px 0px 2px rgba(0,0,0,1)
       text-align center
       border-style solid
       border-radius 0rem 0rem 0.5rem 0.5rem
@@ -140,8 +173,6 @@ export default {
         height 8rem
         color white
     .show
-      transition 0s
       opacity 0.9
-      height 8rem
-      color white
+      height 5rem
 </style>

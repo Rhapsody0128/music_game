5740ba081ecd0a26651486f0e799da0a49ccabb4
Rhapsody0128 <rhapsody0128@gmail.com>
2021-09-07 17:26:54 +0800

    getDataFin

--- mySqlScript.js
+++ mySqlScript.js
@@ -1,26 +1,40 @@
 
 
-  var createData = (dataRow,data) =>{
+  var createData = (dataSheet,data) =>{
     let keys = Object.keys(data)
     let keyStr = ''
     let valueStr = ''
     for(let key of keys){
       keyStr += key + ', '
       if(typeof(data[key])==='string'){
-        valueStr += `'${data[key]}', `
+        valueStr = `'${data[key]}', `
       }else if(typeof(data[key])==='object'){
-        valueStr += `'${JSON.stringify(data[key])}', `
+        valueStr = `'${JSON.stringify(data[key])}', `
       }else{
-        valueStr += `${data[key]}, `
+        valueStr = `${data[key]}, `
       }
     }
     keyStr = keyStr.slice(0, -2)
     valueStr = valueStr.slice(0, -2)
-    let result = `insert into ${dataRow} (${keyStr}) values(${valueStr})`
+    let result = `insert into ${dataSheet} (${keyStr}) values(${valueStr})`
     
     return result
-    
+  }
+
+  var getData = (dataSheet,query) =>{
+    let key = Object.keys(query)
+    let result = 'select * from '
+    let queryStr = ''
+    if(query[key] == undefined){
+      result = result + dataSheet
+    } else{
+      if(typeof(query[key])==='string'){
+        queryStr = `'${query[key]}'`
+      }
+      result = result + `${dataSheet} where ${key} = ${queryStr}`
+    }
+    return result
   }
 
 
-export {createData}
+export {createData,getData}

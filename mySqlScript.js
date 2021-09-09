6e7d0b1e6049d5b4a05b193dca52488019d40806
Rhapsody0128 <rhapsody0128@gmail.com>
2021-09-09 12:58:25 +0800

    fixcreate

--- mySqlScript.js
+++ mySqlScript.js
@@ -7,39 +7,17 @@
     for(let key of keys){
       keyStr += key + ', '
       if(typeof(data[key])==='string'){
-        valueStr = `'${data[key]}', `
+        valueStr += `'${data[key]}', `
       }else if(typeof(data[key])==='object'){
-        valueStr = `'${JSON.stringify(data[key])}', `
+        valueStr += `'${JSON.stringify(data[key])}', `
       }else{
-        valueStr = `${data[key]}, `
+        valueStr += `${data[key]}, `
       }
     }
     keyStr = keyStr.slice(0, -2)
     valueStr = valueStr.slice(0, -2)
     let result = `insert into ${dataSheet} (${keyStr}) values(${valueStr})`
-    
-    return result
-  }
-
-  
-  var createData = (dataSheet,data) =>{
-    let keys = Object.keys(data)
-    let keyStr = ''
-    let valueStr = ''
-    for(let key of keys){
-      keyStr += key + ', '
-      if(typeof(data[key])==='string'){
-        valueStr = `'${data[key]}', `
-      }else if(typeof(data[key])==='object'){
-        valueStr = `'${JSON.stringify(data[key])}', `
-      }else{
-        valueStr = `${data[key]}, `
-      }
-    }
-    keyStr = keyStr.slice(0, -2)
-    valueStr = valueStr.slice(0, -2)
-    let result = `insert into ${dataSheet} (${keyStr}) values(${valueStr})`
-    
+    console.log(result);
     return result
   }
 
